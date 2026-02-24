81b35b4a75936f95ecaae7f4d2a500077060c818
CristianAquino <ycristian10@gmail.com>
2026-02-24 16:48:27 -0500

    first commit

---
+++ src/main/java/com/envoice/api_envoice/ApiEnvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.envoice.api_envoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEnvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEnvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/envoice/api_envoice/ApiEnvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envoice.api_envoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEnvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
