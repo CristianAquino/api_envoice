a4da3440f3d302ec1aec4fdff515fa8f157ba7bf
CristianAquino <ycristian10@gmail.com>
2026-02-25 14:52:54 -0500

    feat: get endpoint for test

--- src/main/java/com/envoice/api_envoice/Controller/EnvoiceController.java
+++ src/main/java/com/envoice/api_envoice/Controller/EnvoiceController.java
@@ -1,4 +1,13 @@
 package com.envoice.api_envoice.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EnvoiceController {
+
+    @GetMapping()
+    private String gretting(){
+        return "grettings";
+    }
 }
