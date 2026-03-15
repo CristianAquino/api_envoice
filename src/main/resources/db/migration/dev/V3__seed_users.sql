1de9341661c6c77d51431a448e1fc8a926c8c1cf
cristianaquino <ycristian10@gmail.com>
2026-03-15 00:05:55 -0500

    feat: dev migrations

---
+++ src/main/resources/db/migration/dev/V3__seed_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(name,email)
+VALUES
+('Admin','admin@test.com'),
+('Juan','juan@test.com');
---
+++ src/main/resources/db/migration/dev/V4__seed_products.sql
@@ -0,0 +1,4 @@
+INSERT INTO products(name,price)
+VALUES
+('Laptop',2500),
+('Mouse',50);
